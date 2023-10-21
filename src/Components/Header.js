b8985ae22bc56a9a019aef740b1aa3d1ae5f7a5b
Anant <aradhyaanant1@gmail.com>
2023-10-21 15:20:06 +0530

    commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,20 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+
 const Header=()=>{
+    const token=localStorage.getItem("token");
+    const username=localStorage.getItem("username");
+      const navigate=useNavigate();
+
+    const handleLogout=()=>{                              //handles the logout functionality//
+            localStorage.removeItem("token");
+            localStorage.removeItem("username");
+            alert("logout successfull");
+             navigate("/login");
+    }
+
+
 return(
    <>
    <nav className="navbar navbar-expand-lg navbar-light bg-primary">
@@ -37,12 +51,24 @@ return(
                                    </li>
                                    </ul>
                                    <div className="div-inline mx-auto my-2 my-lg-0">
+
+
+                                    {token && token!= null ?               // if the user is logged in,then he will definitely have a token and then logout option will be shown to him//
+                                    <>
+                                     <button className="btn btn-primary">Welcome :{username}</button>  
+                                     <button onClick={handleLogout} className="btn btn-primary">Logout</button>  
+                                     </>
+                                     :(                                                 // if the user is not logged in,then login,register option will be shown to him//
+                                        <>
+                                
                                        <Link to={"/login"}>
                                        <button className="btn btn-primary">Login</button>
                                        </Link>
                                        <Link to={"/register"}>
                                        <button className="btn btn-primary">Register</button>
                                        </Link>
+                                       </>
+                                     )}
                                        </div>
                                        </div>
                                        </nav>
