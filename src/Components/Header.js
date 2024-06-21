95470e2604335870b62ba273f331469c5488f7bc
Anant <aradhyaanant1@gmail.com>
2024-06-21 14:13:42 +0530

    commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -24,15 +24,15 @@ return(
            <button
            className="navbar-toggler"
            type="button"
-           data-toggler="collapse"
-           data-target="#navbarsupportContent"
-           aria-controls="navbarsupportContent"
+           data-toggle="collapse"
+           data-target="#navbarSupportedContent"
+           aria-controls="navbarSupported  Content"
            aria-expanded="false"
            aria-label="Toggle navigation"
            >
            <span className="navbar-toggler-icon"></span>
            </button>
-           <div className="collapse-navbar-collapse" id="navbarsupportContent">
+           <div className="collapse navbar-collapse" id="navbarSupportedContent">
                <ul className="navbar-nav mr-auto text-white">
                    <li className="nav-item active">
                        <Link className="nav-link text-white" to="/">
@@ -73,11 +73,6 @@ return(
                                        </div>
                                        </nav>
                                        </>
-
-
-
-
-   
-)
-}
+);
+};
 export default Header;
